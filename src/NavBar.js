8434a4fd6a5791074b6535bae63ad4b3e9835716
Gerome Mandapat <42403342+97gerome@users.noreply.github.com>
2021-07-04 20:47:03 +0800

    Created  NavBar.js and NavBar.css

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+import './NavBar.css'
+
+const NavBar = () => {
+
+    return (
+        <div className="nav-bar">
+            <h1>weather-cast</h1>
+        </div>
+    );
+
+}
+
+export default NavBar;
