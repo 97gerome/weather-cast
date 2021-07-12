7dd181a4294403919abe712f746126b5b5bcf42b
Gerome Mandapat <42403342+97gerome@users.noreply.github.com>
2021-07-12 23:41:00 +0800

    Removed useEffect hooks and imported the convertDateToObject module

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -1,30 +1,24 @@
 import { useState, useEffect } from 'react';
+import convertDateToObject from './modules/convertDateToObject';
 
 import './WeatherCard.css';
 
 const WeatherCard = (props) => {
 
-    const {weatherData} = props;
+    const {location, weatherData} = props;
     const {current} = weatherData;
     const {weather: currentWeather} = current;
-
-    const [location, setLocation] = useState("");
-    const [currentDate, setCurrentDate] = useState({});
-
-    useEffect(() => {
-        const daysArr = ["Sunday", "Monday", "Tuesday", "Thursday", "Friday", "Saturday"];
-        const currentDateObj = new Date(current.dt * 1000);
-        setCurrentDate({day: daysArr[currentDateObj]});
-    }, [])
+    const currentDate = convertDateToObject(current.dt);
 
     return (
         <div className="weather-card">
             <div className="location">{location}</div>
-            <div className="current-date">{currentDate.day}</div>
+            <div className="current-day">{currentDate.day}</div>
+            <div className="current-date">{currentDate.date}</div>
             <div className="current-weather">{currentWeather[0].main}</div>
             <div className="current-temp">{current.temp}</div>
         </div>
-    )
+    );
 }
 
-export default WeatherCard
+export default WeatherCard;
