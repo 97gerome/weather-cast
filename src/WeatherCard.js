f346626bd69916631107726bea708d65de385420
Gerome Mandapat <42403342+97gerome@users.noreply.github.com>
2021-07-13 16:19:18 +0800

    Added default values to props and included weather icon

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -1,3 +1,4 @@
+import { intersection } from 'lodash';
 import { useState, useEffect } from 'react';
 import convertDateToObject from './modules/convertDateToObject';
 
@@ -5,23 +6,39 @@ import './WeatherCard.css';
 
 const WeatherCard = (props) => {
 
-    const {location, weatherData} = props;
-    const {current} = weatherData;
-    const {weather: currentWeather} = current;
-    const currentWeatherDesc = currentWeather[0].description.charAt(0).toUpperCase() + currentWeather[0].description.slice(1);
-    const currentDate = convertDateToObject(current.dt);
+    const {location, weatherData, tempUnit} = props;   
+    const {
+        dt: currentDt, 
+        temp: currentTemp, 
+        weather: currentWeather
+    } = weatherData.current || 
+    {
+        dt: "", 
+        temp: 0, 
+        weather: [{description: "", icon: "01d"}]
+    };
+    const {day: currentDay, date: currentDate, month: currentMonth} = convertDateToObject(currentDt);
+    const [{description: currentWeatherDesc, icon}] = currentWeather;
 
     return (
         <div className="weather-card">
-            <div className="location">{location}</div>
-            <div className="current-weather-container">
-                <div className="current-day">{currentDate.day}</div>
-                <div className="current-date">{currentDate.date}</div>
-                <div className="current-weather">{currentWeatherDesc}</div>
-                <div className="current-temp">{current.temp}</div>
-            </div>
+            {weatherData && weatherData.current && weatherData.current.weather &&
+                <>
+                    <div className="location">{location}</div>
+                    <div className="current-weather-container">
+                        <div className="current-dt">{currentDay}, {currentDate}/{currentMonth}</div>
+                        <div className="current-temp">{currentTemp.toFixed()}°{tempUnit === "metric"? "C" : "F"}</div>
+                        <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`}/>
+                        <div className="current-weather">{currentWeatherDesc.charAt(0).toUpperCase() + currentWeatherDesc.slice(1)}</div>
+                    </div>
+                </>
+            }
         </div>
     );
 }
+
+WeatherCard.defaultProps = {
+
+};
 
 export default WeatherCard;
