c724c486a9e1dc4b174018746a6200e4af82bbea
Gerome Mandapat <42403342+97gerome@users.noreply.github.com>
2021-07-13 13:08:31 +0800

    Nested current weather details in current-weather-container div

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -8,15 +8,18 @@ const WeatherCard = (props) => {
     const {location, weatherData} = props;
     const {current} = weatherData;
     const {weather: currentWeather} = current;
+    const currentWeatherDesc = currentWeather[0].description.charAt(0).toUpperCase() + currentWeather[0].description.slice(1);
     const currentDate = convertDateToObject(current.dt);
 
     return (
         <div className="weather-card">
             <div className="location">{location}</div>
-            <div className="current-day">{currentDate.day}</div>
-            <div className="current-date">{currentDate.date}</div>
-            <div className="current-weather">{currentWeather[0].main}</div>
-            <div className="current-temp">{current.temp}</div>
+            <div className="current-weather-container">
+                <div className="current-day">{currentDate.day}</div>
+                <div className="current-date">{currentDate.date}</div>
+                <div className="current-weather">{currentWeatherDesc}</div>
+                <div className="current-temp">{current.temp}</div>
+            </div>
         </div>
     );
 }
