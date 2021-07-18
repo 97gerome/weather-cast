482a6a9d110a7ce7d360da0a48523c682d6e2733
Gerome Mandapat <42403342+97gerome@users.noreply.github.com>
2021-07-18 18:06:47 +0800

    Added changeHandler function which resets search_input value

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -4,16 +4,24 @@ import './SearchResults.css';
 
 const SearchResults = (props) => {
     
-    const  {searchResults, setWeatherCoordinates} = props;
+    const  {setSearchItem, searchResults, setWeatherCoordinates} = props;
+
+    const changeHandler = (lat, lon, name, country) => {
+        document.getElementById("search_input").value = "";
+        setSearchItem("");
+        setWeatherCoordinates({lat: lat, lon: lon, loc: `${name}, ${country}`})
+    }
 
     return (
         <div className="search-results">
             {searchResults.map((arrItem, index) => {
                 let {name, country, lat, lon} = arrItem;
                 return (
-                    <div className="search-result" key={index} onClick={() => setWeatherCoordinates({lat: lat, lon: lon, loc: `${name}, ${country}`})}>
-                        {name}, {country}
-                        <img src={`https://www.countryflags.io/${country}/flat/64.png`} alt={country}></img>
+                    <div className="search-result" key={index} onClick={() => changeHandler(lat, lon, name, country)}>
+                        <span className="search-result-details">
+                            {name}, {country}
+                            <img src={`https://www.countryflags.io/${country}/flat/64.png`} alt={country}></img>
+                        </span>
                     </div>
                 );
             })}          
