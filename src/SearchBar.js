2f13ddcecf03158da1ad29fb7a327498dc866f42
Gerome Mandapat <42403342+97gerome@users.noreply.github.com>
2021-07-18 18:03:23 +0800

    Added material-icon Search, setSearchItem as a prop for SearchResults and set search_input id to input

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -3,6 +3,7 @@ import debounce from 'lodash.debounce';
 import SearchResults from './SearchResults';
 
 import './SearchBar.css'
+import {Search} from '@material-ui/icons';
 
 const SearchBar = (props) => {
 
@@ -13,12 +14,16 @@ const SearchBar = (props) => {
     , [setSearchItem]);
 
     return (
-        <div className="search-container">
+        <div className="search-wrapper">
             <div className="search-bar">
-                <input type="text" onChange = {e => debouncedSetState(e.target.value)}/>
+                <div className="search-icon-wrapper">
+                    <Search fontSize="small"/>
+                </div>
+                <input type="text" id="search_input" onChange = {e => debouncedSetState(e.target.value)} placeholder="Search by City"/>
             </div>
             {resultsVisibility && 
                 <SearchResults 
+                    setSearchItem = {setSearchItem}
                     searchResults = {searchResults} 
                     setWeatherCoordinates = {setWeatherCoordinates}
                 />
