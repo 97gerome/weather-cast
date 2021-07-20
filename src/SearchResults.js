1565fab28360322325adc8db30f6501807267371
Gerome Mandapat <42403342+97gerome@users.noreply.github.com>
2021-07-20 17:54:13 +0800

    Added react-spring useTransition hook to add a fade effect when SearchResults mounts and unmounts

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,32 +1,36 @@
-import {useEffect} from 'react';
+import { useTransition, animated } from 'react-spring';
 
 import './SearchResults.css';
 
 const SearchResults = (props) => {
     
-    const  {setSearchItem, searchResults, setWeatherCoordinates} = props;
-
-    const changeHandler = (lat, lon, name, country) => {
-        document.getElementById("search_input").value = "";
-        setSearchItem("");
-        setWeatherCoordinates({lat: lat, lon: lon, loc: `${name}, ${country}`})
-    }
+    const  { searchResults, setWeatherCoordinates, resultsVisibility } = props;
+    
+    const transitionFade = useTransition(resultsVisibility, {
+        from: { opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+        config: { duration: 200 }
+    });
 
     return (
-        <div className="search-results">
-            {searchResults.map((arrItem, index) => {
-                let {name, country, lat, lon} = arrItem;
-                return (
-                    <div className="search-result" key={index} onClick={() => changeHandler(lat, lon, name, country)}>
-                        <span className="search-result-details">
-                            {name}, {country}
-                            <img src={`https://www.countryflags.io/${country}/flat/64.png`} alt={country}></img>
-                        </span>
-                    </div>
-                );
-            })}          
-        </div>
-    )
+        transitionFade((style, item) => 
+            item &&
+            <animated.div className="search-results" style={style} >
+                {searchResults.map((arrItem, index) => {
+                    let {name, country, lat, lon} = arrItem;
+                    return (
+                        <div className="search-result" key={index} onClick={() => setWeatherCoordinates({lat: lat, lon: lon, loc: `${name}, ${country}`})}>
+                            <span className="search-result-details">
+                                {name}, {country}
+                                <img src={`https://www.countryflags.io/${country}/flat/64.png`} alt={country}></img>
+                            </span>
+                        </div>
+                    );
+                })}          
+            </animated.div>  
+        )
+    );
 }
 
 export default SearchResults
