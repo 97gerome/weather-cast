f86820bc67c4869435f6f90db6db74e4786a7252
Gerome Mandapat <42403342+97gerome@users.noreply.github.com>
2021-07-20 18:03:34 +0800

    Created CurrentWeather.js React component

---
+++ src/CurrentWeather.js
@@ -0,0 +1,36 @@
+
+import convertDateToObject from './modules/convertDateToObject';
+
+import './CurrentWeather.css';
+
+const CurrentWeather = (props) => {
+
+    const { weatherData, tempUnit } = props;
+    const { timezone_offset: timezoneOffset } = weatherData;
+
+    const {
+        dt: currentDt, 
+        temp: currentTemp, 
+        weather: currentWeather
+    } = weatherData.current || 
+    {
+        dt: "", 
+        temp: 0, 
+        weather: [{description: "", icon: "01d"}]
+    };
+    const {day: currentDay, date: currentDate, month: currentMonth} = convertDateToObject(currentDt, timezoneOffset);
+    const [{description: currentWeatherDesc, icon}] = currentWeather;
+
+    return (
+        <div className="current-weather-container">
+            <div className="current-date">{currentDay}, {currentDate}/{currentMonth}</div>
+            <div className="current-temp">{currentTemp.toFixed()}°{tempUnit === "metric"? "C" : "F"}</div>
+            <div className="current-weather-details">
+                <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="current-weather-icon"/>
+                {currentWeatherDesc.charAt(0).toUpperCase() + currentWeatherDesc.slice(1)}
+            </div>
+        </div>
+    )
+}
+
+export default CurrentWeather
