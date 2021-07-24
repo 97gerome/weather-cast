14e71eacccfe19dac1ee245abfc7e0e81a604634
Gerome Mandapat <42403342+97gerome@users.noreply.github.com>
2021-07-24 20:44:49 +0800

    Removed react-spring library form imports

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,35 +1,24 @@
-import { useTransition, animated } from 'react-spring';
 
 import './SearchResults.css';
 
 const SearchResults = (props) => {
     
-    const  { searchResults, setWeatherCoordinates, resultsVisibility } = props;
-    
-    const transitionFade = useTransition(resultsVisibility, {
-        from: { opacity: 0 },
-        enter: { opacity: 1 },
-        leave: { opacity: 0 },
-        config: { duration: 200 }
-    });
+    const  { style, searchResults, setWeatherCoordinates } = props;
 
     return (
-        transitionFade((style, item) => 
-            item &&
-            <animated.div className="search-results" style={style} >
-                {searchResults.map((arrItem, index) => {
-                    let {name, country, lat, lon} = arrItem;
-                    return (
-                        <div className="search-result" key={index} onClick={() => setWeatherCoordinates({lat: lat, lon: lon, loc: `${name}, ${country}`})}>
-                            <span className="search-result-details">
-                                {name}, {country}
-                                <img src={`https://www.countryflags.io/${country}/flat/64.png`} alt={country}></img>
-                            </span>
-                        </div>
-                    );
-                })}          
-            </animated.div>  
-        )
+        <div className="search-results" style={style} >
+            {searchResults.map((arrItem, index) => {
+                let {name, country, lat, lon} = arrItem;
+                return (
+                    <div className="search-result" key={index} onClick={() => setWeatherCoordinates({lat: lat, lon: lon, loc: `${name}, ${country}`})}>
+                        <span className="search-result-details">
+                            {name}, {country}
+                            <img src={`https://www.countryflags.io/${country}/flat/64.png`} alt={country}></img>
+                        </span>
+                    </div>
+                );
+            })}          
+        </div> 
     );
 }
 
