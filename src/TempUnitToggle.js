e568b89fa028012963788b24dbdb46262cb608a4
Gerome Mandapat <42403342+97gerome@users.noreply.github.com>
2021-07-24 20:45:39 +0800

    Created TempUnitToggle component

---
+++ src/TempUnitToggle.js
@@ -0,0 +1,27 @@
+import './TempUnitToggle.css';
+
+const TempUnitToggle = (props) => {
+
+    const { tempUnit, setTempUnit } = props;
+
+    if(tempUnit === "metric"){
+        return (
+            <div className="temp-unit-toggle-wrapper">
+                <div className="temp-unit-toggle-container">
+                    <span className="current-temp-unit">C</span>
+                    <span className="temp-unit" onClick={() => setTempUnit("imperial")}>F</span>
+                </div>
+            </div>
+        );
+    };
+    return (
+        <div className="temp-unit-toggle-wrapper">
+            <div className="temp-unit-toggle-container">
+                <span className="temp-unit" onClick={() => setTempUnit("metric")}>C</span>
+                <span className="current-temp-unit" >F</span>
+            </div>
+        </div>
+    );
+}
+
+export default TempUnitToggle
