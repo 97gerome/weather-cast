0ee12c02853911a70e114052d9ebf43debb2e4f3
Gerome Mandapat <42403342+97gerome@users.noreply.github.com>
2021-08-15 15:50:18 +0800

    Removed TempUnitToggle component

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -1,39 +1,26 @@
-import Loader from './Loader';
 import CurrentWeather from './CurrentWeather';
 import HourlyForecast from './HourlyForecast';
 import DailyForecast from './DailyForecast';
-import TempUnitToggle from './TempUnitToggle';
+import Background from './Background';
+
+import convertDateToObject from './modules/convertDateToObject';
 
 import './WeatherCard.css';
 
-const currentUnitStyle = {
-    backgroundColor: "rgb(255, 255, 255, 0.8",
-    color: "black"
-}
-
 const WeatherCard = (props) => {
 
-    const {location, isWeatherCardLoading, weatherData, tempUnit, setTempUnit} = props;
+    const { location, weatherData, tempUnit } = props;
 
-    if(isWeatherCardLoading){
-        return (
-            <div className="weather-card">
-                <div className="loader-wrapper">
-                    <Loader />
-                </div>
-            </div>
-        );
-    };
     return(
         <div className="weather-card">
-            <div className="location">{location}</div>
-            <CurrentWeather weatherData={weatherData} tempUnit={tempUnit}/>
-            <HourlyForecast weatherData={weatherData}/>
-            <DailyForecast weatherData={weatherData}/>
-            <TempUnitToggle 
-                tempUnit={tempUnit}
-                setTempUnit={setTempUnit}
+            <Background 
+                weatherId={weatherData.current.weather[0].id}
+                time={convertDateToObject(weatherData.current.dt, weatherData.timezone_offset).hour}
             />
+            <div className="location">{location}</div>
+            <CurrentWeather currentData={weatherData.current} tempUnit={tempUnit} timezoneOffset={weatherData.timezone_offset}/>
+            <HourlyForecast hourlyData={weatherData.hourly} timezoneOffset={weatherData.timezone_offset}/>
+            <DailyForecast dailyData={weatherData.daily} timezoneOffset={weatherData.timezone_offset}/>
         </div>
     );
 }
